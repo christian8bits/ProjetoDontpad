377fd12e6640d169b703719f67106251674d801d
christian8bits@gmail.com <christian8bits@gmail.com>
2019-09-27 10:15:27 -0300

    Commit Fotos clicaveis

--- app/src/main/java/com/christian/projetodontpad/MainActivity.java
+++ app/src/main/java/com/christian/projetodontpad/MainActivity.java
@@ -2,11 +2,17 @@ package com.christian.projetodontpad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,20 +23,48 @@ public class MainActivity extends AppCompatActivity {
 
     public void irParaFotos(View view){
         FrameLayout flf = findViewById(R.id.frameFotos);
-        FrameLayout flt =  findViewById(R.id.frameText);
+        FrameLayout flt = findViewById(R.id.frameText);
+        FrameLayout fli = findViewById(R.id.frameVerImage);
         flf.setVisibility(View.VISIBLE);
         flt.setVisibility(View.GONE);
+        fli.setVisibility(View.GONE);
     }
 
 
     public void irParaTexto(View view){
         FrameLayout flf = findViewById(R.id.frameFotos);
         FrameLayout flt = findViewById(R.id.frameText);
+        FrameLayout fli = findViewById(R.id.frameVerImage);
         flf.setVisibility(View.GONE);
+        fli.setVisibility(View.GONE);
         flt.setVisibility(View.VISIBLE);
-
     }
 
+
+    public void irVerFoto(View view) {
+        FrameLayout flf = findViewById(R.id.frameFotos);
+        FrameLayout flt = findViewById(R.id.frameText);
+        FrameLayout fli = findViewById(R.id.frameVerImage);
+        flf.setVisibility(View.GONE);
+        flt.setVisibility(View.GONE);
+        fli.setVisibility(View.VISIBLE);
+        ImageView img = findViewById(R.id.imageViewFull);
+        ImageView img3 = findViewById(R.id.imageView1);
+        img.setImageDrawable(img3.getDrawable());
+    }
+
+
+    public void irVerFoto2(View view) {
+        FrameLayout flf = findViewById(R.id.frameFotos);
+        FrameLayout flt = findViewById(R.id.frameText);
+        FrameLayout fli = findViewById(R.id.frameVerImage);
+        flf.setVisibility(View.GONE);
+        flt.setVisibility(View.GONE);
+        fli.setVisibility(View.VISIBLE);
+        ImageView img = findViewById(R.id.imageViewFull);
+        ImageView img3 = findViewById(R.id.imageView2);
+        img.setImageDrawable(img3.getDrawable());
+    }
 
 
 
